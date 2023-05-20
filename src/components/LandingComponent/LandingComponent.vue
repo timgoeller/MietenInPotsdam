68f30e121d3209c02f42e595ee72effdbf89feb2
TimGoeller <tim.goeller@posteo.de>
2023-05-20 20:19:12 +0200

    Basic project config

---
+++ src/components/LandingComponent/LandingComponent.vue
@@ -0,0 +1,7 @@
+<script setup>
+console.log('hello script setup')
+</script>
+
+<template>
+    <p class="p-8 m-3 text-red-400">Test</p>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import LandingComponent from './components/LandingComponent/LandingComponent.vue'
 
-createApp(App).mount('#app')
+
+const routes = [
+    { path: '/', component: LandingComponent },
+  ]
+
+const router = createRouter({
+// 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+history: createWebHashHistory(),
+routes, // short for `routes: routes`
+})
+
+app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
